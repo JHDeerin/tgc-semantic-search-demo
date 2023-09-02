58683d4400c593ef4f769054af44c8aa6809ffb1
JHDeerin <2016deerinjo@gmail.com>
2023-09-01 23:23:20 -0400

    added function to fetch + parse TGC article pages

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -1,4 +1,5 @@
 """Tests related to searching/scraping TGC articles."""
+from datetime import datetime
 import json
 from pathlib import Path
 
@@ -11,6 +12,12 @@ import tgc_semantic_search_demo.scraper
 def example_tgc_search() -> dict:
     with open(Path(__file__).parent / "example_tgc_search.json") as file:
         return json.load(file)
+
+
+@pytest.fixture()
+def example_tgc_article() -> str:
+    with open(Path(__file__).parent / "example_tgc_article.html") as file:
+        return file.read()
 
 
 def test_correct_article_urls_extracted_from_search(example_tgc_search):
@@ -27,3 +34,35 @@ def test_correct_article_urls_extracted_from_search(example_tgc_search):
 def test_searching_recent_tgc_articles():
     search_results_html = tgc_semantic_search_demo.scraper.search_tgc(max_posts=5)
     assert len(search_results_html) > 0
+
+
+def test_parsing_article_content(example_tgc_article):
+    article_data = tgc_semantic_search_demo.scraper.parse_article(example_tgc_article)
+    expected = tgc_semantic_search_demo.scraper.ArticleData(
+        title="Gen Z Will Not Save the World",
+        publish_datetime=datetime.fromisoformat("2023-09-01T04:04:17+00:00"),
+        author="Callum MacLeod",
+        text=[],
+        url="https://www.thegospelcoalition.org/article/gen-z-not-save-world/",
+    )
+    assert article_data.title == expected.title
+    assert article_data.publish_datetime == expected.publish_datetime
+    assert article_data.author == expected.author
+    assert len(article_data.text) > 0
+    assert article_data.url == expected.url
+
+
+def test_fetching_article_content():
+    article_data = tgc_semantic_search_demo.scraper.fetch_article("https://www.thegospelcoalition.org/article/gen-z-not-save-world/")
+    expected = tgc_semantic_search_demo.scraper.ArticleData(
+        title="Gen Z Will Not Save the World",
+        publish_datetime=datetime.fromisoformat("2023-09-01T04:04:17+00:00"),
+        author="Callum MacLeod",
+        text=[],
+        url="https://www.thegospelcoalition.org/article/gen-z-not-save-world/",
+    )
+    assert article_data.title == expected.title
+    assert article_data.publish_datetime == expected.publish_datetime
+    assert article_data.author == expected.author
+    assert len(article_data.text) > 0
+    assert article_data.url == expected.url
--- tgc_semantic_search_demo/scraper.py
+++ tgc_semantic_search_demo/scraper.py
@@ -1,14 +1,26 @@
 """Utilities for scraping article data from The Gospel Coalition website."""
+from datetime import datetime
 import re
+from dataclasses import dataclass
 from typing import List
 
 import requests
 from bs4 import BeautifulSoup
 
 
+@dataclass
+class ArticleData:
+    """Data from a TGC article post."""
+    title: str
+    author: str
+    publish_datetime: datetime
+    text: List[str]
+    url: str
+
+
 def parse_article_urls(html: str) -> List[str]:
     """Return all the TGC article URLs that appear in the given HTML."""
-    soup = BeautifulSoup(html)
+    soup = BeautifulSoup(html, features="html.parser")
     article_urls = soup.find_all("a", href=re.compile("/article"))
     return [url["href"] for url in article_urls]
 
@@ -18,4 +30,31 @@ def search_tgc(max_posts: int) -> str:
     search_url = f"https://www.thegospelcoalition.org/wp-content/themes/sage/tgc-ajax.php?action=search_load_more&post_id=&args%5Bs%5D=&args%5Border%5D=DESC&args%5Borderby%5D=post_date&page=1&posts_per_page={max_posts}"
     # User-Agent header required, or else we get a 403 error
     res = requests.get(search_url, headers={"User-Agent": "Requests/2.31.0"})
+    assert res.status_code < 400
     return res.json()["data"]["html"]
+
+
+def parse_article(article_html: str) -> ArticleData:
+    soup = BeautifulSoup(article_html, features="html.parser")
+    title = soup.find("title").text
+    author = soup.find("article").find("span", class_="author").text
+    paragraphs = soup.find("article").find_all("p")
+    publish_datetime = datetime.fromisoformat(
+        soup.find("article").find("time")["datetime"]
+    )
+    article_url = soup.find("link", rel=re.compile("canonical"))["href"]
+
+    return ArticleData(
+        title=title,
+        author=author.strip(),
+        publish_datetime=publish_datetime,
+        text=[p.text for p in paragraphs],
+        url=article_url
+    )
+
+
+def fetch_article(url: str) -> ArticleData:
+    """Fetch the given article's data from the TGC website."""
+    res = requests.get(url)
+    assert res.status_code < 400
+    return parse_article(res.text)
