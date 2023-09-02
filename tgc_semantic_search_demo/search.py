3158cebe641454696e5121d3c994d9ef1c6e81e8
JHDeerin <2016deerinjo@gmail.com>
2023-09-02 02:00:49 -0400

    added basic semantic search w/ ChromaDB

--- tgc_semantic_search_demo/search.py
+++ tgc_semantic_search_demo/search.py
@@ -1,4 +1,5 @@
 """Utilities for searching through TGC articles using AI semantic search methods."""
+import argparse
 import json
 import os
 from pathlib import Path
@@ -13,11 +14,11 @@ import chromadb
 
 def initialize_database(src: Path):
     # TODO: Implement this!
-    client = chromadb.Client()
+    client = chromadb.PersistentClient("./_chromadb_cache")
 
     collection = client.create_collection("tgc-articles")
     # Load each saved TGC article into memory and add it to the DB
-    for i, path in enumerate(os.listdir(src)[:5]):
+    for i, path in enumerate(os.listdir(src)):
         if not (src / path).is_file():
             continue
         with open(src / path) as file:
@@ -27,11 +28,26 @@ def initialize_database(src: Path):
             metadatas=[{"article": article_json["url"], "paragraph": i} for i in range(len(article_json["text"]))],
             ids=[f"{article_json['url']}_{i}" for i in range(len(article_json["text"]))]
         )
-        print(f"[{i+1}]Added '{path}'")
+        print(f"[{i+1}/{len(os.listdir(src))}]Added '{path}'")
     return collection
 
 
+def print_search_results(results: chromadb.QueryResult):
+    for i in range(len(results["ids"][0])):
+        print("-" * 40)
+        print(f"{i+1}. {results['metadatas'][0][i]}")
+        print(f"\tDistance: {results['distances'][0][i]}")
+        print(f"\tText: '{results['documents'][0][i]}'")
+        print()
+
+
 if __name__ == "__main__":
-    db = initialize_database(src=Path("./_article_cache"))
-    results = db.query(query_texts=["is it okay for christians to work as spies"], n_results=5)
-    print(results)
+    # db = initialize_database(src=Path("./_article_cache"))
+    db = chromadb.PersistentClient("./_chromadb_cache").get_collection("tgc-articles")
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("query")
+    parser.add_argument("-n", "--num-results", type=int, required=False, default=5)
+    args = parser.parse_args()
+    results = db.query(query_texts=[args.query], n_results=5)
+    print_search_results(results)
