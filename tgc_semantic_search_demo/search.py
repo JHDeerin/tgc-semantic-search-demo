568c8fca4397a3f29b1b57aa7d65fd7816648fca
JHDeerin <2016deerinjo@gmail.com>
2023-09-02 18:24:50 -0400

    reformatted search result display

--- tgc_semantic_search_demo/search.py
+++ tgc_semantic_search_demo/search.py
@@ -2,6 +2,8 @@
 import argparse
 import json
 import os
+import time
+from datetime import datetime
 from pathlib import Path
 
 # these three lines swap the stdlib sqlite3 lib with the pysqlite3 package
@@ -13,48 +15,61 @@ import chromadb
 import chromadb.utils.embedding_functions
 
 
-def initialize_database(src: Path) -> chromadb.Collection:
-    # TODO: Implement this!
+def initialize_database(src: Path, refresh: bool=False) -> chromadb.Collection:
     client = chromadb.PersistentClient("./_chromadb_cache")
-
-    collection = client.create_collection(
+    collection = client.get_or_create_collection(
         "tgc-articles",
         embedding_function=chromadb.utils.embedding_functions.SentenceTransformerEmbeddingFunction(model_name="all-mpnet-base-v2", device="cuda")
     )
+    if not refresh:
+        return collection
+
     # Load each saved TGC article into memory and add it to the DB
-    for i, path in enumerate(os.listdir(src)):
-        if not (src / path).is_file():
-            continue
-        with open(src / path) as file:
+    filepaths = [src / path for path in os.listdir(src) if (src / path).is_file()]
+    for i, path in enumerate(filepaths):
+        with open(path) as file:
             article_json = json.load(file)
         collection.add(
             documents=article_json["text"],
-            metadatas=[{"article": article_json["url"], "paragraph": i} for i in range(len(article_json["text"]))],
-            ids=[f"{article_json['url']}_{i}" for i in range(len(article_json["text"]))]
+            metadatas=[
+                {
+                    "article": article_json["title"],
+                    "author": article_json["author"],
+                    "date": article_json["publish_datetime"],
+                    "url": article_json["url"],
+                    "paragraph": i
+                }
+                for i in range(len(article_json["text"]))
+            ],
+            ids=[
+                f"{article_json['url']}_{i}" for i in range(len(article_json["text"]))
+            ]
         )
-        print(f"[{i+1}/{len(os.listdir(src))}]Added '{path}'")
+        print(f"[{i+1}/{len(filepaths)}] Added '{path}'")
     return collection
 
 
 def print_search_results(results: chromadb.QueryResult):
     for i in range(len(results["ids"][0])):
         print("-" * 40)
-        print(f"{i+1}. {results['metadatas'][0][i]}")
-        print(f"\tDistance: {results['distances'][0][i]}")
-        print(f"\tText: '{results['documents'][0][i]}'")
+        data = results['metadatas'][0][i]
+        print(f"{i+1}. {data['article']} ({datetime.fromisoformat(data['date']).date().isoformat()}) ({results['distances'][0][i]:.2f})")
+        print(results['documents'][0][i])
         print()
 
 
 if __name__ == "__main__":
-    # db = initialize_database(src=Path("./_article_cache"))
-    db = chromadb.PersistentClient("./_chromadb_cache").get_collection(
-        "tgc-articles",
-        embedding_function=chromadb.utils.embedding_functions.SentenceTransformerEmbeddingFunction(model_name="all-mpnet-base-v2", device="cuda")
-    )
-
+    print("Parsing args")
     parser = argparse.ArgumentParser()
     parser.add_argument("query")
+    parser.add_argument("-r", "--refresh", action="store_true")
     parser.add_argument("-n", "--num-results", type=int, required=False, default=5)
     args = parser.parse_args()
-    results = db.query(query_texts=[args.query], n_results=5)
+
+    print("Loading DB")
+    db = initialize_database(src=Path("./_article_cache"), refresh=args.refresh)
+    print("Running search")
+    start_time = time.time()
+    results = db.query(query_texts=[args.query], n_results=args.num_results)
     print_search_results(results)
+    print(f"({time.time() - start_time:.3f} seconds)")
